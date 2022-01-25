bd630d81a1aa4d86d4bd6221c22a0b72d2b83328
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-01-25 13:20:16 -0800

    Added menu open and close on click

--- main.js
+++ main.js
@@ -2,3 +2,20 @@ if (typeof $ == 'undefined'){
     console.log('oops! I still have to link my jQuery properly!');
   } else {console.log('I did it! I linked jQuery and this js file properly!')};
   
+  const openMenu = function(e) {
+    console.log('open')
+    $(".menu-open").css("display", "block");
+    $(".menu-closed").css("display", "none");
+    e.preventDefault();
+  };
+
+  const closeMenu = function(e) {
+    console.log('close')
+    $(".menu-open").css("display", "none");
+    $(".menu-closed").css("display", "flex");
+    e.preventDefault();
+  };
+
+  $('.hamburger-menu' ).on('click', openMenu);
+
+  $('#x-icon' ).on('click', closeMenu);
