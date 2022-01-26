fa49f7804556ecdd9571a82992843d4c804be2fc
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-01-25 16:59:03 -0800

    updating code to link at the beginning of my hmtl, and adding slideToggle

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+$(() => {
 if (typeof $ == 'undefined'){
     console.log('oops! I still have to link my jQuery properly!');
   } else {console.log('I did it! I linked jQuery and this js file properly!')};
   
   const openMenu = function(e) {
     console.log('open')
-    $(".menu-open").css("display", "block");
-    $(".menu-closed").css("display", "none");
+    // $(".menu-open").css("display", "block");
+    // $(".menu-closed").css("display", "none");
+    $('.menu-open').slideToggle();
     e.preventDefault();
   };
 
@@ -19,3 +21,5 @@ if (typeof $ == 'undefined'){
   $('.hamburger-menu' ).on('click', openMenu);
 
   $('#x-icon' ).on('click', closeMenu);
+
+});
